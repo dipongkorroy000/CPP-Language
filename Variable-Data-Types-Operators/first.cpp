2d7d9573d66853d5a151f5ee13c6cdb88d4f61c6
dipongkor000 <dipongkorroy000@gmail.com>
2025-03-21 19:14:44 +0600

    first code of cpp

---
+++ Variable-Data-Types-Operators/first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "first line" << "\n";
+    cout << "second line\n";
+    cout << "third line\nfourth line\n";
+    cout << "six line" << " seven line";
+    return 0;
+}
