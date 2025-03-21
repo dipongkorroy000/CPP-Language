712eb79c50f55aa0cb2584feebcd4f2126b994e9
dipongkor000 <dipongkorroy000@gmail.com>
2025-03-21 20:02:40 +0600

    add file

---
+++ Variable-Data-Types-Operators/dataType.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout << "Hello World!" << endl;
+    // cout << "first line" << "\n";
+    // cout << "second line\n";
+    // cout << "third line\nfourth line\n";
+    // cout << "six line" << " seven line";
+
+
+    // primitive data type---
+    // int age = 25;
+    // cout << age << endl;
+    // cout << sizeof(age) << "byte" << endl;
+
+    // char grade = 'A';
+    // cout << grade << endl;
+    // cout << sizeof(grade) << "byte" << endl;
+
+    // float PI = 3.1416f;
+    // cout << PI << endl;
+
+    // bool booleanData = true;
+    // cout << booleanData << endl;
+
+    // double price = 100.256;
+    // cout << price << endl;
+    return 0;
+}
